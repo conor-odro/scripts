5f1f63fa2714d615f26bada316b2ab7e770e33c3
Conor Watson <conor@odro.co.uk>
2021-02-03 11:28:12 +0000

    feat: create getAuthCode helper

---
+++ bullhorn-api/library/getAuthCode.js
@@ -0,0 +1,22 @@
+import fetch from 'node-fetch';
+
+const getAuthCode = async ({ clientId, username, password }) => {
+  const params = new URLSearchParams({
+    client_id: clientId,
+    response_type: 'code',
+    action: 'Login',
+    username,
+    password,
+  });
+
+  const response = await fetch(`https://auth.bullhornstaffing.com/oauth/authorize?${params}`);
+
+  if (!response || response.status !== 200) throw new Error('Failed to fetch AuthCode, please check parameters');
+
+  const { searchParams } = new URL(response.url);
+  const code = searchParams.get('code');
+
+  return code;
+};
+
+export default getAuthCode;
